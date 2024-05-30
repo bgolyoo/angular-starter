04c578a1d520c7f47544cfc39899d05457079b3d
Gergely Bodor <bgolyoo@gmail.com>
2024-05-30 22:31:18 +0200

    feat: add husky with lint-staged and commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,6 @@ const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommen
 module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
-    ignores: ['.angular', '.vscode', '.idea', 'dist/', 'node_modules/'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
